d12c1d35b457180d8eb8989fa2a33a306abeeb65
Mocha Adam Fadly <adamfadly11@gmail.com>
2021-03-19 17:02:56 +0700

    clean up and adding new components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import SideBar from "./Components/SideBar/SideBar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideBar />
+      <div>
+        <h1>Header</h1>
+        <h1>Body</h1>
+      </div>
     </div>
   );
 }
