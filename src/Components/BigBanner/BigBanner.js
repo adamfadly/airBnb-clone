abf6abc2fba44330c475e1a95add0211091e42dd
Mocha Adam Fadly <adamfadly11@gmail.com>
2021-03-20 10:48:38 +0700

    adding new banner

--- src/Components/BigBanner/BigBanner.js
+++ src/Components/BigBanner/BigBanner.js
@@ -7,9 +7,25 @@ function BigBanner() {
       <div className="banner_info">
         <h1>Your World is Worth Sharing</h1>
         <p>Turn your Extra space into your next opportunity</p>
-        <button className="button_banner">Become Host</button>
+        <button className="button_banner">
+          <h5>Become Host</h5>
+        </button>
       </div>
     </div>
+
+    // <div>
+    //   <div class="bigWrapper">
+    //     <img
+    //       src="https://media.cntraveler.com/photos/5db1d0dd11c1e500092e7133/master/pass/airbnb-ski-aspen-28328347.jpg"
+    //       alt=""
+    //     />
+    //   </div>
+    //   <div className="Content">
+    //     <h1>Your World is Worth Sharing</h1>
+    //     <p>Turn your Extra space into your next opportunity</p>
+    //     <button className="button_banner">Become Host</button>
+    //   </div>
+    // </div>
   );
 }
 
